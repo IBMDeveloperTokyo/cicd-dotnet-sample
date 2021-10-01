ff520886de4700239310207978568918ebbba659
Tetsuya Kawano <tetsuya.kawano@gmail.com>
2021-10-01 18:27:41 +0900

    add dotnet sample app and test

---
+++ SampleApp.Tests/HomePageOnGet.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using SampleApp;
+
+using Xunit;
+
+namespace SampleApp.Tests
+{
+    public class HomePageOnGet : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public HomePageOnGet(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsHomePageWithProductListing()
+        {
+            // Arrange & Act
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Contains("Welcome", stringResponse);
+            Assert.Contains("OpenShift", stringResponse);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
